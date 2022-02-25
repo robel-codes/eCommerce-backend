bb7d12c068d1d7f3547ff56589d5d30316c27e5c
Robel <rofikre@yahoo.com>
2022-02-25 17:16:53 -0500

    created api route for tag model

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,71 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{
+        through: ProductTag,
+        model: Product
+    }]
+  }).then(rows => {
+    res.status(200).json(rows);
+  }).catch(err => {
+    return res.status(400).json({ 'error': err });
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const id = req.params.id;
+  Tag.findOne({
+      where: { id },
+      include: [{
+          through: ProductTag,
+          model: Product
+      }]
+  }).then(rows => {
+      res.status(200).json(rows);
+  }).catch(err => {
+      return res.status(400).json({ 'error': err });
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  const { tag_name } = req.body;
+  Tag.create({ tag_name })
+    .then(row => {
+      res.status(200).json(row);
+    }).catch(err => {
+      return res.status(400).json({ 'error': err });
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  const { id } = req.params;
+  const { tag_name } = req.body;
+  Tag.update({ tag_name }, {
+      where: {
+          id
+      }
+  }).then(isUpdated => {
+      res.status(200).send(isUpdated + "");
+  }).catch(err => {
+      return res.status(400).json({ 'error': err });
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  const id = req.params.id;
+  Tag.destroy({
+      where: { id }
+  }).then(isDeleted => {
+      res.status(200).send("" + isDeleted);
+  }).catch(err => {
+      return res.status(400).json({ 'error': err });
+  });
 });
 
 module.exports = router;
